81469ef070c07513b8206e4195aade52eb79e270
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2022-01-10 12:32:39 -0300

    tailwind installed and configured

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document'
+// import 'tailwindcss/tailwind.css'
 
 export default class MyDocument extends Document {
     render() {
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div className={`
+      flex
+      h-screen
+      justify-center
+      items-center
+    `}>
+      <span>Home</span>
+    </div>
+  )
+}
