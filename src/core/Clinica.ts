246d694a04bba243f2787dd7eb203484cd7d1fea
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2022-07-13 15:57:53 -0300

    clinics ids formated

--- src/core/Clinica.ts
+++ src/core/Clinica.ts
@@ -4,15 +4,51 @@ export default class ClinicasCheckModel {
     // #selecionado: boolean
 
     constructor(titulo: string) {
-        if (titulo.includes(' ')) {
-            this.#id = titulo.split(' ', 1)[0].toLowerCase()
-        }
-        if (titulo.includes('/')) {
-            this.#id = titulo.split('/', 1)[0].toLowerCase()
-        }
-        this.#id = titulo.toLowerCase()
+        this.id = titulo
+        // if (titulo.includes(' ')) {
+        //     this.#id = titulo.split(' ', 1).toString().toLowerCase()
+        //     // console.log('ID:', this.id)
+        // }
+        // if (titulo.includes('/')) {
+        //     this.#id = titulo.split('/', 1).toString().toLowerCase()
+        //     // console.log('ID:', this.id)
+        // }
+        // this.#id = titulo.toLowerCase()
+        // console.log('ID:', this.id)
         this.#titulo = titulo
         // this.#selecionado = selecionado
+    }
+
+    set id(title) {
+        
+        let tituloModificado = title
+        
+        if (tituloModificado.includes(' ') || tituloModificado.includes('/')) {
+            if (tituloModificado.includes(' ')) {
+                tituloModificado = tituloModificado.split(' ', 1).toString().toLowerCase()
+                this.#id = tituloModificado.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+                console.log('titulo modificado com espaço: ', this.id)
+            }
+            if (tituloModificado.includes('/')) {
+                tituloModificado = tituloModificado.split('/', 1).toString().toLowerCase()
+                this.#id = tituloModificado.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+                console.log('titulo modificado com barra: ', this.id)
+            }
+            
+            // console.log('ID:', this.id)
+        } else {
+            this.#id = tituloModificado.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+            console.log('titulo com palavra unica: ', this.id)
+
+        }
+        
+        // switch (tituloModificado) {
+        //     case tituloModificado.includes(' '):
+        //         tituloModificado = tituloModificado.split(' ', 1).toString().toLowerCase()
+        //         this.#id = tituloModificado
+        //         break
+
+        // }
     }
 
     get id() {
